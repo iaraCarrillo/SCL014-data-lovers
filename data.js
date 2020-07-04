18d460488407a930c3c48eb923a5c7cb04910f66
iara-carrillo <iaracarrillo.r@gmail.com>
2020-07-04 11:56:50 -0400

    Avances del Readme

--- data.js
+++ data.js
@@ -1,3 +1,9 @@
+/* Función de buscar
+export const searchPokemons = (pokemonList, inputValue) => pokemonList.filter(
+    (pokemon) => pokemon.name.toLowerCase().startsWith(inputValue),
+  );
+*/
+
 // estas funciones son de ejemplo
 
 export const example = () => 'example';
