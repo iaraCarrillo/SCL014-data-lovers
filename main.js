bc3d6c6887c12f1ddd77ed3175391616d60bc991
Angie <angie@Angelinas-MacBook-Air.local>
2020-07-13 13:06:22 -0400

    pruebaQuinta

--- main.js
+++ main.js
@@ -1,29 +1,23 @@
-//solo para llamar la data al pagina de main
+//Se impora (llama) la data a main.js
 import basePokemon from './data/pokemon/pokemon.js';
-// importando las funciones desde data.js
+//Se importan las funciones (Filtros) que se encuentran ubicados en data.js a main.js
 import { filterCandy, generation, filterType, filterSearch} from './data.js'
-////convirtiendo el objeto pokemon a un array
+//Se convierte el objeto pokemon en un array
 const dataPokemon = basePokemon.pokemon;
-//console.log (dataPokemon)
-function enterbtn() {
-    document.getElementById("root").style.display = "block";//mostrar
-}
- 
-function toPage() {
-    document.getElementById("root").style.display = "none"; //esconder
-}
-  
-///////imprimiendo los personajes en la segunda página luego del evento click ////
 
-// constante que contiene información del atributo ID donde se va a imprimir en el html
+///IMPRIMIR TARJETAS en Contenedor (root)///
+//Se crean constantes que contienen información del atributo ID, donde se va a imprimir html por medio de Javascript
 const container = document.getElementById("root")
-//for que recorrerá el largo de la data
+const foto = document.getElementsByClassName("foto");
+//For que recorrerá el largo de toda la data
 for (let i = 0; i < dataPokemon.length; i++) {
-  let pokemon = dataPokemon[i]//con esto le decimos al computador que ahora el pokemon que estamos viendo es el que esta en este ciclo
+//Con esto le decimos al computador que ahora el pokemon que estamos viendo es el que esta en este ciclo
+  let pokemon = dataPokemon[i]
+//Se declaran variables con información que usaremos en nuestra tarjeta
   let name= pokemon.name;
   let img= pokemon.img;
   let num= pokemon.num;
-  
+//Variabe "imprimir" que contendrá nuestra información visible para la tarjeta (Html)
   let imprimir = `
   <section id="tarjetas">
     <div class="starTarjeta">
@@ -31,136 +25,130 @@ for (let i = 0; i < dataPokemon.length; i++) {
         <label for="radio2">★</label>
       </button>
     </div>
-    <img id="${name}" src=${img} class=foto>
+    <img id="${name}" src=${img} class="foto">
     <div class="infoPokemon">
         <p>${num}.${name}<p> 
     </div>
   </section>`
-  //se imprime llamando a la nueva variable según lo especificado en la variable anterior
+//Se imprime llamando a la nueva variable según lo especificado en la variable anterior
   container.innerHTML += imprimir
 
-
-  /////Modal/////
-  //Obtener el modal
+///MODAL TARJETA///
+//Constante para obtener el modal
   const modal = document.getElementById("modal");
-  //Abrir el modal
-  const foto = document.getElementsByClassName("foto");
-  //console.log("Foto", foto)
-  //foto.addEventListener ('click',modalImpri)
+//For que recorrerá nuestra clase foto
   for (let i = 0; i <foto.length; i++) {
+//A nuestra variable img le asignamos la lista "foto"
     let img = foto[i];
+//Se llama a img por medio del evento click se imprimen la tarjeta
     img.addEventListener('click', modalImpri)
-    //console.log("imprimir", img)
   }
-  let namePokemonSelected;
-  let numPokemonSelected;
-  let imgPokemonSelected;
-  let typePokemonSelected;
-  let countOfCandys=[];
-  let quickMoves=[];//son listas de movimientos rapidos, tambien se les llama arrays
-  let specialAttacks=[];
-  let generationPokemonSelected;
-  let weaknessesPokemonSelected;
-  let resistantPokemonSelected;
-  function modalImpri(event) {
-    modal.innerHTML ="";//se inicializa modal
-    const pokemonSelected = dataPokemon.find(function (personaje) {
-      if(personaje.name == event.target.id){
-        console.log(personaje.name);//este personaje.name es el event.target.id que es el nombre
-        //let siglePokemon = new pokemon;
-        namePokemonSelected = personaje.name;
-        numPokemonSelected = personaje.num;
-        imgPokemonSelected = personaje.img;
-        typePokemonSelected = personaje.type;
-        generationPokemonSelected = personaje.generation["name"];
-        weaknessesPokemonSelected = personaje.weaknesses;
-        resistantPokemonSelected = personaje.resistant;
-        if(personaje.evolution["nextEvolution"]!=undefined){
-          console.log(personaje.evolution["nextEvolution"])
-            let contadorDulces = personaje.evolution["nextEvolution"][0].candycost;
-          console.log("contador de dulces",contadorDulces);
-            countOfCandys.push(contadorDulces)
-            countOfCandys=contadorDulces;
-          console.log(countOfCandys)
-        }else{
-          countOfCandys="No evoluciona";
-        }
-        for (let i = 0; i < personaje.quickmove.length; i++){
-          let movimientoRapido = personaje.quickmove[i];
-          //console.log(movimientoRapido.name);
-          quickMoves.push(movimientoRapido.name)
-        }
-        for (let i = 0; i < personaje.specialattack.length; i++){
-          let ataqueEspecial = personaje.specialattack[i];
-          //console.log(ataqueEspecial.name);
-          specialAttacks.push(ataqueEspecial.name)
-        }
-        return personaje
+}
+//Mostramos todos los pokemon
+let namePokemonSelected;//Nombre
+let numPokemonSelected;//Número
+let imgPokemonSelected;//Imagen
+let typePokemonSelected;//Tipo
+let countOfCandys=[];//son listas de caramelos, tambien se les llama arrays
+let quickMoves=[];//son listas de movimientos rapidos, tambien se les llama arrays
+let specialAttacks=[];//son listas de ataques especiales, tambien se les llama arrays
+let generationPokemonSelected;//Generación
+let weaknessesPokemonSelected;//Debilidad
+let resistantPokemonSelected;//Resistencia
+function modalImpri(event) {
+  modal.innerHTML ="";//Se inicializa modal (.innerHTML devuelve o establece la sintaxis HTML)
+  //Find devuelve el valor del primer elemento del array que cumple la función de prueba proporcionada
+  const pokemonSelected = dataPokemon.find(function (personaje) {
+  //personaje.name es el event.target.id que es el nombre
+    if(personaje.name == event.target.id){ //Comparación estricta 
+  //Se asignan variables con la información a visualizar
+      namePokemonSelected = personaje.name; //Nombre
+      numPokemonSelected = personaje.num; //Número
+      imgPokemonSelected = personaje.img; //Imagen
+      typePokemonSelected = personaje.type; //Tipo
+      generationPokemonSelected = personaje.generation["name"]; //Generación
+      weaknessesPokemonSelected = personaje.weaknesses; //Debilidad
+      resistantPokemonSelected = personaje.resistant; //Resistencia
+      //Capas hasta llegar a candycost
+      if(personaje.evolution["nextEvolution"]!=undefined){
+        console.log(personaje.evolution["nextEvolution"])
+          let contadorDulces = personaje.evolution["nextEvolution"][0].candycost;
+        console.log("contador de dulces",contadorDulces);
+          countOfCandys.push(contadorDulces)
+          countOfCandys=contadorDulces;
+        console.log(countOfCandys)
+      }else{
+        countOfCandys="No evoluciona";
       }
-      //return personaje.name === event.target.id
-      
-    })
-    
-    modal.innerHTML +=
-    `<div class="card-modal" id="card-modal">
-    <div class="imgPokemon">
-      <img src=${imgPokemonSelected} class="imgModal">
-      <p>Tipo:</p>
-      <p class="modalInline">${typePokemonSelected}</p>
-      <p>Huevos:</p>
-      <p class="modalInline">${countOfCandys}</p>
-    </div>
-    <div class="infoPokemonModal">
-    <section id="cards">
-      <div>
-      <button id="close" class="close">x</button>
-      </div>
-      <section id="letras">
-        <div class="numeroNombre">
-          <p>${numPokemonSelected}</p> 
-          <p>${namePokemonSelected}</p>
-        </div>
-        <div class="generacion">
-          <p>Generación: ${generationPokemonSelected}</p>
-        </div>
-        <div class="especificaciones">  
-          <p>Resistencia:</p>
-          <p class="modalInlineWhite">${resistantPokemonSelected}</p>
-          <p>Debilidad:</p>  
-          <p class="modalInlineWhite">${weaknessesPokemonSelected}</p>         
-          <p>Movimiento rápido:</p>
-          <p class="modalInlineWhite">${quickMoves}</p>
-          <p>Movimiento especial:</p>  
-          <p class="modalInlineWhite">${specialAttacks}</p>           
-        </div>
-    </div>
-        </section>
-      </section>
-    </div>`
-      console.log(quickMoves)
-      modal.classList.add('modal--show')
-  
-      modal.addEventListener('click', (e) => {
-      if (e.target.classList.contains('modal')) modal.classList.remove('modal--show')
-      });
-
-//CERRAR TARJETA
-      function closeModal (){
-        let buttonClose = document.getElementById("close");
-        buttonClose.addEventListener("click", function(){
-        modal.style.display = "none";
-//      container.style.display = "block";
-        });
+      //Por medio de un for se recorren todos los movimientos rápidos de un pokémon
+      for (let i = 0; i < personaje.quickmove.length; i++){
+        let movimientoRapido = personaje.quickmove[i];
+      //Push añade uno o más elementos al final de un array y devuelve la nueva
+        quickMoves.push(movimientoRapido.name)
+      }
+      //Por medio de un for se recorren todos los ataques especiales de un pokémon
+      for (let i = 0; i < personaje.specialattack.length; i++){
+        let ataqueEspecial = personaje.specialattack[i];
+      //Push añade uno o más elementos al final de un array y devuelve la nueva
+        specialAttacks.push(ataqueEspecial.name)
+      }
+      return personaje
     }
-    closeModal();
-  } 
+    //return personaje.name === event.target.id 
+  })
+  //.innerHTML devuelve o establece la sintaxis HTML
+  modal.innerHTML += //Operador +=, abreviación de suma
+  `<div class="card-modal" id="card-modal">
+  <div class="imgPokemon">
+    <img src=${imgPokemonSelected} class="imgModal">
+    <p>Tipo:</p>
+    <p class="modalInline">${typePokemonSelected}</p>
+    <p>Huevos:</p>
+    <p class="modalInline">${countOfCandys}</p>
+  </div>
+  <div class="infoPokemonModal">
+  <section id="cards">
+    <div>
+    <button id="close" class="close">x</button>
+    </div>
+    <section id="letras">
+      <div class="numeroNombre">
+        <p>${numPokemonSelected}</p> 
+        <p>${namePokemonSelected}</p>
+      </div>
+      <div class="generacion">
+        <p>Generación: ${generationPokemonSelected}</p>
+      </div>
+      <div class="especificaciones">  
+        <p>Resistencia:</p>
+        <p class="modalInlineWhite">${resistantPokemonSelected}</p>
+        <p>Debilidad:</p>  
+        <p class="modalInlineWhite">${weaknessesPokemonSelected}</p>         
+        <p>Movimiento rápido:</p>
+        <p class="modalInlineWhite">${quickMoves}</p>
+        <p>Movimiento especial:</p>  
+        <p class="modalInlineWhite">${specialAttacks}</p>           
+      </div>
+  </div>
+      </section>
+    </section>
+  </div>` 
+
+  //CERRAR TARJETA
+  function closeModal (){
+    let buttonClose = document.getElementById("close");
+    buttonClose.addEventListener("click", function(){
+    modal.style.display = "none";
+    });
+  }
+  closeModal();
 }
 
-//Llamar Filtros
+///LLAMAR FILTROS///
 //FILTRO CARAMELOS
-  // constante que contiene información del atributo ID
+  //Constante que contiene información del atributo ID
   const typeCandy = document.getElementById("candy");
-  //llamando la constante para que cuando escuche el evento ejecute la función
+  //Llamando la constante para que cuando escuche el evento ejecute la función
   typeCandy.addEventListener("change", () => {
   //variable que alamacena el valor de la opción seleccionada por el usuario
   let selectCandy = typeCandy.options[typeCandy.selectedIndex].value
@@ -175,9 +163,6 @@ for (let i = 0; i < dataPokemon.length; i++) {
     let nameByCandy = positionCandy[i].name
     let numByCandy = positionCandy[i].num
     let imgByCandy = positionCandy[i].img
-    //console.log(nameByCandy)
-    //variable que me almacena el recorrido el valor del recorrido de la debilidades y todo lo que contiene adentro  de (type)
-    
     //variable que contiene los valores a imprimir
     let prinType = `
     <section id="tarjetas">
@@ -243,7 +228,7 @@ for (let i = 0; i < dataPokemon.length; i++) {
     //se imprime llamando a la nueva variable según lo especificado en la variable anterior
     container.innerHTML += `<p>${prinType}<p>`
   }
-  //For que recorre la clase  foto de mi linea de impresión
+  //For que recorre la clase foto de mi linea de impresión
   for (let i = 0; i < foto.length; i++) {
     let img = foto[i];
     img.addEventListener('click', modalImpri)
@@ -255,21 +240,22 @@ for (let i = 0; i < dataPokemon.length; i++) {
 });
 
 
-// Función buscar por nombre de pokémon
-//llamo al boton con el id para que cuando escuche el evento ejecute la función
+//FILTRO BUSCA POKEMON
+//llamo al botón con el id para que cuando escuche el evento ejecute la función
 document.getElementById('okBtn').addEventListener('click', () => {
   // se llama a la constante que donde se imprime en el html y se declara vacia para que limpie la página
   document.getElementById('root').innerHTML = '';
   //variable que almacena el valor el nombre del pokemon escrito por el usuario
-  const name = document.getElementById('search-pokemon').value;
-  //constante que primero (charAt) obtiene, con el nombre, al personaje en la cadena + tomo el nombre en array
-  const finalName = name.charAt().toUpperCase() + name.slice(1).toLowerCase();
+  const name = document.getElementById('searchPokemon').value;
+  //Se asigna variable
+  const finalName = name.toLowerCase();//Se pasan las letras a minusculas para que se puedan buscar en la data
   //variable que almacena la función buscar por nombre de pokemón exportada desde data.js con sus parámetros
   let chosenName = (filterSearch(dataPokemon,finalName));
   //for que recorrerá el largo de la data
   for (let i = 0; i <chosenName.length; i++) {
-    //variable que me almacena el valor del recorrido hecho por el for
-    let dataName = chosenName[i]
+    let nameBySearch = chosenName[i].name
+    let numBySearch = chosenName[i].num
+    let imgBySearch = chosenName[i].img
     //variable que contiene los valores a imprimir
     let printName =`
     <section id="tarjetas">
@@ -278,14 +264,15 @@ document.getElementById('okBtn').addEventListener('click', () => {
           <label for="radio2">★</label>
         </button>
       </div>
-      <img id="${dataName.name}" src=${dataName.img} class=foto>
+      <img id="${nameBySearch}" src=${imgBySearch} class=foto>
       <div class="infoPokemon">
-          <p>${dataName.num}.${dataName.name}<p> 
+          <p>${numBySearch}.${nameBySearch}<p> 
       </div>
     </section>`
     //se imprime llamando a la nueva variable según lo especificado en la variable anterior
     container.innerHTML += `<p>${printName}<p>`
   }
+  console.log(foto)
     //For que recorre la clase  foto de mi linea de impresión
     for (let i = 0; i <foto.length; i++) {
       let img = foto[i];
