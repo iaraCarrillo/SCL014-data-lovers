946e4c168ba7690e9d61b45533f0c17aa893625e
iara-carrillo <iaracarrillo.r@gmail.com>
2020-07-18 20:56:32 -0400

    Respaldo

--- data.js
+++ data.js
@@ -1,63 +1,55 @@
 ///EXPORTANDO las función de filtar por caramelos///
 //se fijan los parámetros de la función
 export const filterCandy = (data, property, selectCandy) => {
-    ///se declara una constante tipo array
-    const resultado = [];
-    //for me recorrerá la data de proyecto
-    for (let i = 0; i < data.length; i++) {
-     //for que recorrerá el atributo tipo array de debilidad
-    
-      let costOfEvolution =data[i].evolution["nextEvolution"]
-      console.log (data[i].evolution["nextEvolution"]);
+  ///se declara una constante tipo array
+  const resultado = [];
+  //for me recorrerá la data de proyecto
+  for (let i = 0; i < data.length; i++) {
+   //for que recorrerá el atributo tipo array de debilidad
+  
+    let costOfEvolution =data[i].evolution["nextEvolution"]
+    console.log (data[i].evolution["nextEvolution"]);
     if(data[i].evolution["nextEvolution"]!=undefined){
-      console.log (costOfEvolution[i].candycost);
+    console.log (costOfEvolution[i].candycost);
       if(costOfEvolution[i].candycost == selectCandy)
         //se llama a la constante tipo array declarada al incio y una vez recorrida la longitud de la data
         // se hace un push para alamcene todos los elementos los devuelve la nueva longitud del array.
         resultado.push(data[i]) 
       }
     }
-    
-    return resultado
-  };
   
+  return resultado
+};
   
-  
-  ///EXPORTANDO las función de filtar por generacion///
-  export const filterGeneration = (data, selectGeneration) => {
-    console.log(selectGeneration);
-    const resultado = data.filter(pokemon => pokemon.generation.name === selectGeneration);
-   // console.log(resultado)
-    return resultado
-  };
-  
-
-
-
-
-  
-  ///EXPORTANDO las función de filtar por tipo///
-  export const filterType = (data, property, selectType) => {
-    const resultado = [];
-    for (let i = 0; i < data.length; i++) {
-      for (let a = 0; a < data[i][property].length; a++) {
-        //console.log (data[i][property][a]);
-        if (data[i][property][a] == selectType) {
-          resultado.push(data[i])
-        }
+///EXPORTANDO las función de filtar por tipo///
+export const filterType = (data, property, selectType) => {
+  const resultado = [];
+  for (let i = 0; i < data.length; i++) {
+    for (let a = 0; a < data[i][property].length; a++) {
+      //console.log (data[i][property][a]);
+      if (data[i][property][a] == selectType) {
+        resultado.push(data[i])
       }
     }
-   // console.log(resultado)
-    return resultado
-  };
-  
-  
-  ///EXPORTANDO la función buscar por nombre de pokémon///
-  export const filterSearch = (data,searchBox) =>{
-    const search = data.filter((element) => {
-      return element.name.includes(searchBox);
-    });
-console.log(search)
-    return search;
   }
- 
+  // console.log(resultado)
+  return resultado
+};
+
+
+///EXPORTANDO la función buscar por nombre de pokémon///
+export const filterSearch = (data,searchBox) =>{
+  const search = data.filter((element) => {
+    return element.name.includes(searchBox);
+  });
+  return search;
+}
+
+
+///EXPORTANDO las función de filtar por generacion///
+export const filterGeneration = (data, selectGeneration) => {
+  //console.log(selectGeneration);
+  const resultado = data.filter(pokemon => pokemon.generation.name === selectGeneration);
+ // console.log(resultado)
+  return resultado
+};
